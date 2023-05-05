395fade321782137964c34aabb9a5cded67ff26e
pabon98 <hasinanjumpabon@gmail.com>
2023-05-05 09:19:40 +0600

    added

---
+++ _deploysteps.js
@@ -0,0 +1,15 @@
+/* 
+1. add vercel config file
+2. vercel
+3. add: "build": node index.js , in your package.json file
+*/
+
+/* 
+1. First type vercel in command line
+2. Which scope do you want to deploy? pabon
+3. Link to existing project? [y/n] n
+4. What's your project name? genius-car-server
+
+5.In which directory is your code located? ./(that menas our project is in root folder means it is in index.js) click enter
+6. After successfully deployed we will add environment variables in vercel Environment variables. You will find it in project settings.
+*/
